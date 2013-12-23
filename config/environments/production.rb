6ba322255978429d80a4dd760bdd4f061fa1586c
Daniel Åsljung <daniel.asljung@gmail.com>
2013-12-23 18:36:46 +0100

    assets compiled for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,12 @@ LearnRails::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # email enabled in production
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] } 
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.raise_delivery_errors = false 
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
